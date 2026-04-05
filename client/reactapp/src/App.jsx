09a9d4e82b61a470d78c91eb365c01462385f46d
CadeM2 <cademcmillan2@gmail.com>
2026-04-04 23:29:01 -0400

    Added setting page with light/dark mode display setting

--- client/reactapp/src/App.jsx
+++ client/reactapp/src/App.jsx
@@ -3,12 +3,14 @@ import LoginPage from './pages/LoginPage.jsx'
 import SignupPage from './pages/SignupPage.jsx'
 import DashboardPage from './pages/DashboardPage.jsx'
 import AdminDashboardPage from './pages/AdminDashboardPage.jsx'
+import SettingsPage from './pages/SettingsPage.jsx'
 
 export default function App() {
   return (
     <Routes>
       <Route path="/admin" element={<AdminDashboardPage />} />
       <Route path="/dashboard" element={<DashboardPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
       <Route path="/signup" element={<SignupPage />} />
       <Route path="/" element={<LoginPage />} />
       <Route path="*" element={<Navigate to="/" replace />} />
